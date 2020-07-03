7078e75dba2905b0f66c412bc4a49880dcf25ab4
Maciej <maciej.skwara2@gmail.com>
2020-07-03 10:23:50 +0200

    update

--- agents/prob.py
+++ agents/prob.py
@@ -26,7 +26,22 @@ class LocAgent:
         self.size = size
         self.walls = walls
         # list of valid locations
+
+        # tutaj trzeba wrzucic orientacje
+        dir_to_idx = {'N': 0, 'E': 1, 'S': 2, 'W': 3}
+        #
         self.locations = list({*locations(self.size)}.difference(self.walls))
+        loc_with_orientation = []
+        for i in self.locations:
+            for idx2 in dir_to_idx.values():
+                new = (i[0], i[1],idx2)
+                loc_with_orientation.append(new)
+                # print(new)
+
+        print(len(loc_with_orientation))
+        print(loc_with_orientation)
+        print(len(self.locations))
+        print(self.locations)
         # dictionary from location to its index in the list
         self.loc_to_idx = {loc: idx for idx, loc in enumerate(self.locations)}
         self.eps_perc = eps_perc
@@ -35,11 +50,54 @@ class LocAgent:
         # previous action
         self.prev_action = None
 
-        self.P = None
+        # self.dir = dir
+        self.t = 0
+        prob = 1.0 / len(self.locations)
+        self.P = prob * np.ones([len(self.locations)], dtype=np.float)
 
     def __call__(self, percept):
         # update posterior
         # TODO PUT YOUR CODE HERE
+        T = np.zeros([len(self.locations), len(self.locations)], dtype=np.float)
+        if self.prev_action == 'forward':
+            for index, loc in enumerate(self.locations):
+                #next_loc = nextLoc(loc, self.dir)
+                next_loc = nextLoc(loc, 'E')
+                if legalLoc(next_loc, self.size) and (next_loc not in self.walls):
+                    next_index = self.loc_to_idx[next_loc]
+                    T[index, next_index] = 1.0 - self.eps_move
+                    T[index,index] = self.eps_move
+
+                else:
+                    T[index,index] = 1.0
+        else:
+
+            for index, loc in enumerate(self.locations):
+                T[index,index] = 1.0
+
+        # print(np.shape(T))
+        # print(T)
+        O = np.zeros([len(self.locations)], dtype=np.float)
+        for index, loc in enumerate(self.locations):
+            prob = 1.0
+            for d in ['N', 'E', 'S', 'W']:
+                nh_loc = nextLoc(loc, d)
+
+                obstale = (not legalLoc(nh_loc, self.size)) or (nh_loc in self.walls)
+
+                if obstale == (d in percept):
+                    prob *= (1 - self.eps_perc)
+                else:
+                    prob *= self.eps_perc
+            O[index] = prob
+
+
+        self.t += 1
+
+        self.P = T.transpose() @ self.P
+        self.P = O * self.P
+        self.P /= np.sum(self.P)
+
 
 
         # -----------------------
@@ -63,11 +121,18 @@ class LocAgent:
         P_arr = np.zeros([self.size, self.size, 4], dtype=np.float)
 
         # put probabilities in the array
+        # metoda ma zwracac macierz z wartosciami rozkladu o wymiarach: [size, size, 4]
+        # 4 wartosci, po jednej dla kazdego kierunku
+
         # TODO PUT YOUR CODE HERE
+        dir_to_idx = {'N': 0, 'E': 1, 'S': 2, 'W': 3}
+        for idx2 in dir_to_idx.values():
+            for idx, loc in enumerate(self.locations):
+                P_arr[loc[0], loc[1]] = self.P[idx]
+                # print(self.P[idx])
 
-
+        # print(P_arr)
         # -----------------------
-
         return P_arr
 
     def forward(self, cur_loc, cur_dir):
